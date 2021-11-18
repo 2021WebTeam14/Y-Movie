b876d3356bfbad024ce8ea47ef0a602ca320b3cd
배재익 <0308bae@naver.com>
2021-11-18 12:22:16 +0900

    todo

--- ymovie/src/main/java/api_DB/apiDAO.java
+++ ymovie/src/main/java/api_DB/apiDAO.java
@@ -14,13 +14,30 @@ import movie.*;
 public class apiDAO {
 	
 	private static String getTagValue(String tag, Element eElement) {
-	    NodeList nlList = eElement.getElementsByTagName(tag).item(0).getChildNodes();
+	    Node preNList = eElement.getElementsByTagName(tag).item(0);
+	    if(preNList == null) 
+	        return null;
+	    NodeList nlList = preNList.getChildNodes();
 	    Node nValue = (Node) nlList.item(0);
 	    if(nValue == null) 
 	        return null;
 	    return nValue.getNodeValue();
 	}
 
+	private static String getTagValueDirec(String tag, Element eElement) {
+		String asdf=null;
+		
+	    NodeList preNList = eElement.getElementsByTagName(tag);
+	    for (int i = 0; i < preNList.getLength(); i++) {
+		    NodeList nlList = preNList.item(i).getChildNodes().item(0).getChildNodes();
+		    Node nValue = (Node) nlList.item(0);
+		    if(nValue == null) 
+		        return null;
+			System.out.println(nValue.getNodeValue());
+		}
+	    return asdf;	    
+	}
+	
 	public ArrayList<movieDTO> getAPIAboutMovie() throws Exception {
 		ArrayList<movieDTO> dtos = new ArrayList<movieDTO>();
 		
@@ -40,7 +57,7 @@ public class apiDAO {
 		preUrl.append("&itemPerPage=100&curPage=");
 		
 		StringBuilder url = new StringBuilder(preUrl.toString());
-		for(int j = 1; j < size+1; j++) {
+		for(int j = 1; j < 2/*size+1*/; j++) {
 			System.out.printf("API Loading ( %d00 / %d )\n", j, size*100+last);
 			url = new StringBuilder(preUrl.toString());
 			url.append(j);
@@ -53,7 +70,10 @@ public class apiDAO {
 			for(int temp = 0; temp < nList.getLength(); temp++){
 				nNode = nList.item(temp);
 				if(nNode.getNodeType() == Node.ELEMENT_NODE){
-					
+					if (temp != 0) {
+						getTagValueDirec("director", eElement);
+						System.out.println("##################################");						
+					}
 					eElement = (Element) nNode;
 					movieDTO dto = new movieDTO(getTagValue("movieNm", eElement), getTagValue("movieCd", eElement), 
 									Integer.parseInt(getTagValue("prdtYear", eElement) == null ? "0" : getTagValue("prdtYear", eElement)), getTagValue("prdtStatNm", eElement), 
